27160c5f687cdf8238b70036392ea4f8c9f0d344
itsshawondevs <shawonuser24@gmail.com>
2024-05-05 21:08:28 +0600

    sidebar complete

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ import Registration from './pages/auth/Registration';
 import Home from './pages/home/Home';
 import RootLayout from './components/layout/RootLayout';
 import Error from './pages/error/Error';
+import Message from './pages/message/Message';
+import Notification from './pages/notification/Notification';
+import Settings from './pages/settings/Settings';
 
 const App = () => {
 
@@ -19,6 +22,9 @@ const App = () => {
       <Route>
           <Route element={<RootLayout />}>
               <Route path='/home' element={<Home />} />
+              <Route path='/message' element={<Message />} />
+              <Route path='/notification' element={<Notification />} />
+              <Route path='/settings' element={<Settings />} />
           </Route>
           <Route path='/' element={<Login />} />
           <Route path='/registration' element={<Registration />} />
--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,20 +1,52 @@
 import React from 'react'
+import './sidebar.css'
+
 import Avatar from '@mui/material/Avatar';
 import { NavLink } from 'react-router-dom';
+import { IoHomeOutline } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoSettingsOutline } from "react-icons/io5";
+import { LuLogOut } from "react-icons/lu";
+
 
 const Sidebar = () => {
   return (
-    <section style={{background: '#5F35F5', marginTop: '50px', marginLeft: '35px', marginBottom: '20px', padding: '24px', borderRadius: '20px'}}>
-      <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'}}>
+    <section className='sidebarmain'>
+      <div className='sidebarinner'>
         <div>
-          <div style={{height: '100px', width: '100px', overflow: 'hidden'}}>
-            <Avatar alt="Shawon" src="/static/images/avatar/2.jpg" style={{height: '100%', width: '100%', objectFit: 'cover', fontSize: '40px'}} />
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <Avatar alt="Shawon" src="/static/images/avatar/2.jpg" style={{height: '100px', width: '100px', fontSize: '40px',}} />
           </div>
           <div style={{marginTop: '78px'}}>
-              <ul>
-                <li><NavLink></NavLink></li>
+              <ul className='sidebarItem'>
+                <li>
+                  <NavLink to='/home'>
+                    <IoHomeOutline />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/message'>
+                    <AiOutlineMessage />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/notification'>
+                    <IoMdNotificationsOutline />
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to='/settings'>
+                    <IoSettingsOutline   />
+                  </NavLink>
+                </li>
               </ul>
           </div>
+        </div>
+        <div>
+          <button style={{background: 'transparent', border: 'none', padding: '8px 50px', cursor: 'pointer'}}>
+            <LuLogOut className='logout'/>
+            </button>
         </div>
       </div>
     </section>
